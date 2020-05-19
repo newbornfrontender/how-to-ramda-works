e4f669885ad81b847d3568817a96ff648e776a68
Sergey <newbornfrontender@gmail.com>
2020-05-19 22:12:38 +0300

    feat(testing): add new test for "add" method

--- src/add/add.spec.ts
+++ src/add/add.spec.ts
@@ -1,10 +1,32 @@
+// @ts-nocheck
+
+import * as fc from 'fast-check';
+
 import { add } from './add';
 
 describe('add method', () => {
   it('pass correct with numbers', () => {
     expect.hasAssertions();
+    fc.assert(fc.property(fc.integer(), fc.integer(), (a, b) => {
+      expect(add(a, b)).toStrictEqual(a + b);
+      expect(add(a)(b)).toStrictEqual(a + b);
+    }));
+  });
 
-    expect(add(1, 2)).toStrictEqual(3);
-    expect(add(2)(11)).toStrictEqual(13);
+  it('fails with strings', () => {
+    expect.hasAssertions();
+    fc.assert(fc.property(fc.string(1, 10), fc.string(1, 10), (a, b) => {
+      expect(add(a, b)).toBeNaN();
+    }));
+  });
+
+  it('works with random specs', () => {
+    expect.hasAssertions();
+    expect(add(4, '7')).toStrictEqual(11);
+    expect(add('2', 5)).toStrictEqual(7);
+    expect(add(true, false)).toStrictEqual(1);
+    expect(add(null, null)).toStrictEqual(0);
+    expect(add(undefined, undefined)).toBeNaN();
+    expect(add(new Date(4), new Date(2))).toStrictEqual(6);
   });
 });
