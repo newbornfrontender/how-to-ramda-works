98b02953a5d81c552f7c6fb4c4abbb46f07ab164
Sergey <newbornfrontender@gmail.com>
2020-05-19 16:06:15 +0300

    docs(add): add docs for "add" method

--- src/add/add.ts
+++ src/add/add.ts
@@ -1,9 +1,9 @@
-export const add = (...args: number[]): any => {
-  if (args.length === 1) return (v: number): any => add(args[0], v);
-  return args.reduce((acc, v) => Number(acc) + Number(v));
-};
+// export const add = (...args: number[]): any => {
+//   if (args.length === 1) return (v: number): any => add(args[0], v);
+//   return args.reduce((acc, v) => Number(acc) + Number(v));
+// };
 
-// export function add(a: number, b?: number): any {
-//   if (arguments.length === 1) return (c: number): any => add(a, c);
-//   return Number(a) + Number(b);
-// }
+export function add(a: number, b?: number): any {
+  if (arguments.length === 1) return (c: number): any => add(a, c);
+  return Number(a) + Number(b);
+}
---
+++ src/add/readme.md
@@ -0,0 +1,17 @@
+# Using
+
+Adds two values
+
+## Definitions
+
+```ts
+  add(a: number, b?: number): number;
+  add(a: number): (b: number) => number;
+```
+
+## Example
+
+```js
+add(2, 4); // => 6
+add(3)(8); // => 11
+```
